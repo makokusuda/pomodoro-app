790a53e958b4aefe1b8b7842f487a350d51f3dde
Mako Kusuda <mako.kusuda@gmail.com>
2021-02-19 09:50:48 +0900

    Modify start/stop timer

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,51 +1,89 @@
 import React, { useRef, useState, useEffect } from "react";
 
 const Timer = () => {
-  const initialNum = 10;
+  const initialPomodoro = 25;
+  let selectedTime = 0;
   let countDown = null;
-  let time = initialNum;
-  const timer = useRef();
+  let time = initialPomodoro;
+  let timerEl = useRef(null);
+  const startButton = useRef(null);
+  const stopButton = useRef(null);
 
   useEffect(() => {
-    timer.current.textContent = time;
+    timerEl.current.textContent = initialPomodoro;
+    startButton.current.style.display = "block";
+    stopButton.current.style.display = "none";
   }, []);
 
   const runTimer = () => {
     if (time > 0) {
       time--;
     } else {
-      clearInterval(countDown);
+      stopTimer();
     }
-    timer.current.textContent = time;
+    timerEl.current.textContent = time;
   };
 
   const startTimer = () => {
+    startButton.current.style.display = "none";
+    stopButton.current.style.display = "block";
     countDown = setInterval(() => runTimer(), 1000);
   };
 
   const stopTimer = () => {
     clearInterval(countDown);
+    startButton.current.style.display = "block";
+    stopButton.current.style.display = "none";
   };
 
   const resetTimer = () => {
-    time = initialNum;
-    timer.current.textContent = time;
+    if (selectedTime === 0) {
+      time = initialPomodoro;
+    } else {
+      time = selectedTime;
+    }
+    timerEl.current.textContent = time;
   };
 
-  const convertTime = (sec) => {
-    if (sec < 60) {
-      return sec + "sec";
+  const dropDown = (start, end, interval) => {
+    const content = [];
+    for (let i = start; i <= end; i += interval) {
+      content.push(
+        <option key={i} value={i}>
+          {i}
+        </option>
+      );
     }
-    return Math.floor(sec / 60) + "min" + (sec % 60) + "sec";
+    return content;
+  };
+
+  const handleOnChange = (e) => {
+    timerEl.current.textContent = e.target.value;
+    time = e.target.value;
+    selectedTime = e.target.value;
   };
 
   return (
     <div>
       This is timer
-      <div ref={timer}></div>
-      <button onClick={startTimer}>Start</button>
-      <button onClick={stopTimer}>Stop</button>
+      <div ref={timerEl}></div>
+      <button ref={startButton} onClick={startTimer}>
+        Start
+      </button>
+      <button ref={stopButton} onClick={stopTimer}>
+        Stop
+      </button>
       <button onClick={resetTimer}>Reset</button>
+      <div>
+        Pomodoro length : Minutes
+        <select
+          name="pomodoro-length"
+          onChange={handleOnChange}
+          defaultValue={initialPomodoro}
+        >
+          {dropDown(1, 60, 1)}
+        </select>
+      </div>
     </div>
   );
 };
